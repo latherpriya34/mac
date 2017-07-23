5f12dc9ffc2aeb0a1b3300203ad289417cf9f48a
Priya <Priya@DESKTOP-5TPR3M1.fios-router.home>
2017-07-23 15:06:24 -0400

    Add job detail

---
+++ CollegeRegisteration/src/com/collegeregistration/student/JobDetail.java
@@ -0,0 +1,51 @@
+package com.collegeregistration.student;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class jobdetail
+ */
+@WebServlet("/JobDetail")
+public class JobDetail extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public JobDetail() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ProcessStudent p = new ProcessStudent();
+		String student_id= request.getParameter("studentid");
+		String job_title = request.getParameter("jobtitle");
+		String internship_type = request.getParameter("internshiptype");
+		String job_group = request.getParameter("jobgroup");
+		String job_description = request.getParameter("description");
+		String r= p.insertjobdetail(student_id, job_title, internship_type, job_group, job_description);
+		PrintWriter out = response.getWriter();
+		out.println(r);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
